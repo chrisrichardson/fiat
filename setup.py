2e5e153f453c147e90649d309646c504e3fdcada
Chris Richardson <chris@bpi.cam.ac.uk>
2020-09-25 18:20:19 +0100

    Fix issues with version

--- setup.py
+++ setup.py
@@ -14,11 +14,10 @@ if sys.version_info < (3, 6):
 
 setup(name="fenics-fiat",
       description="FInite element Automatic Tabulator",
-      version=version,
       author="Robert C. Kirby et al.",
       author_email="fenics-dev@googlegroups.com",
       url="https://bitbucket.org/fenics-project/fiat/",
-      use_scm_version={'write_to': 'ffcx/version.py',
+      use_scm_version={'write_to': 'FIAT/version.py',
                        'write_to_template': '__version__ = "{version}"\n',
                        'parentdir_prefix_version': 'fenics-ffcx-'},
       setup_requires=["setuptools_scm"],
